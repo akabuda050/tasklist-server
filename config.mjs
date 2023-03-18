2055970f1e6a4c810dc8fae5e863f6eb6722c47e
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-03-18 01:23:56 +0100

    fix resolving certs

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-import { fs, path } from "./fsUtils.mjs";
+import { fs, path, __dirname } from "./fsUtils.mjs";
 
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 // Config dotenv in order to read .env file.
@@ -10,8 +10,8 @@ export const registrationSecrets = {
 export const environment = process.env.environment || "dev";
 export const serverPort = process.env.server_port || 8080;
 export const privateKey = process.env.private_key
-  ? fs.readFileSync(path.resolve(process.env.private_key))
+  ? fs.readFileSync(path.resolve(__dirname, process.env.private_key))
   : "";
 export const certificate = process.env.certificate
-  ? fs.readFileSync(path.resolve(process.env.certificate))
+  ? fs.readFileSync(path.resolve(__dirname, process.env.certificate))
   : "";
